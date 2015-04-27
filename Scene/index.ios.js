4648e464160b16b60541a7c7d2a1fd02c7866087
Ben Shaw <benshaw9@gmail.com>
2015-04-26 17:16:23 -0700

    Following the tutorial...

--- Scene/index.ios.js
+++ Scene/index.ios.js
@@ -5,52 +5,91 @@
  * https://github.com/facebook/react-native
  */
 
-
 'use strict';
 var foo = require('./foo.js');
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var MOCKED_STORIES_DATA = [];
+var sf = {
+  name: 'San Francisco',
+  location: 'USA',
+  image: 'http://clubzone.com/content/uploads/2014/12/san-fran.jpg'
+};
+for (var i=0; i<25; i++) {
+  MOCKED_STORIES_DATA.push(sf);
+};
+
 var Scene = React.createClass({
-  render: function() {
+
+  getInitialState: function() {
+    var dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    });
+    return {
+      dataSource: dataSource.cloneWithRows(MOCKED_STORIES_DATA),
+    }
+  },
+
+  renderPlace: function(place) {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js {`${foo(42)}`}
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
+        <Image
+          source={{uri: place.image}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.name}>{place.name}</Text>
+          <Text style={styles.location}>{place.location}</Text>
+        </View>
       </View>
     );
+  },
+
+  render: function() {
+    debugger;
+    return <ListView
+      dataSource={this.state.dataSource}
+      renderRow={this.renderPlace}
+      style={styles.listView}
+    />
   }
 });
 
 var styles = StyleSheet.create({
+  listView: {
+    backgroundColor: '#222222',
+  },
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
+    backgroundColor: '#88AADD',
     alignItems: 'center',
-    backgroundColor: '#33BBFF',
+    marginBottom: 1,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer: {
+    flex: 1,
   },
-  instructions: {
+  name: {
+    fontSize: 10,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  location: {
+    fontSize: 8,
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 275,
+    height: 60,
   },
 });
 
